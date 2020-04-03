5d8a8e32723998d065df2e8692a693269bf7d881
Zurab Sajaia <16375540+vavalomi@users.noreply.github.com>
2020-04-03 18:20:01 -0400

    adding get_export script

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Utilities to work with Survey Solutions API
 
 
-* Generate SQL database from the (Tabular) file export
+## Generate SQL database from the (Tabular) file export
 
 call it from the command line:
 ```shell
@@ -13,4 +13,10 @@ or in your script:
 import tabular_to_sql
 
 tabular_to_sql.convert("example_1_Tabular.zip",  "sqlite:///example_1.db", "quest.json")
+```
+
+## Download export file and generate SQL database
+
+```shell
+python ./get_export.py 'https://demo.mysurvey.solutions' api_user pass1234
 ```
---
+++ get_export.py
@@ -0,0 +1,83 @@
+
+import argparse
+import sys
+import os
+import time
+from tqdm import tqdm
+from tabular_to_sql import convert
+
+
+def get_export(client, qid, qversion, exportpath=None):
+
+    if not exportpath:
+        exportpath = "."
+
+    qidentity = qid + "$" + str(qversion)
+
+    response = client.export.get_info(qidentity)
+    if not response['HasExportedFile']:
+        _ = client.export.start(qidentity)
+        response = client.export.get_info(qidentity)
+        pbar = tqdm(total=100, unit="%")
+        oldval = 0
+        while response["RunningProcess"]:
+            time.sleep(1)
+            delta = response["RunningProcess"]["ProgressInPercents"] - oldval
+            oldval = response["RunningProcess"]["ProgressInPercents"]
+            pbar.update(delta)
+            response = client.export.get_info(qidentity)
+        pbar.update(100-oldval)
+        pbar.close()
+
+    zipfile = client.export.get(qidentity, exportpath=exportpath)
+    docobj = client.questionnaires.document(qid, qversion)
+
+    filename = os.path.basename(zipfile)
+    filename, _ = os.path.splitext(filename)
+    conn_url = "sqlite:///" + os.path.join(exportpath, filename + ".sqlite")
+    convert(zipfile, conn_url, docobj)
+
+def process_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="URL of your Headquarters app")
+    parser.add_argument("username", help="API user name")
+    parser.add_argument("password",help="API user password")
+    parser.add_argument("--qid", help="Questionnaire Id")
+    parser.add_argument("--qversion", help="Questionnaire Version")
+    parser.add_argument("--exportpath", help="Destination folder")
+
+    return parser.parse_args()
+
+def get_questionnaire(client):
+    qlist= client.questionnaires()
+    c_qlist = len(qlist)
+    for i in range(c_qlist):
+        print("[{i}] {title}, version {version}".format(i=i+1, title=qlist[i].title, version=qlist[i].version))
+
+    print()
+    msg = "Pick a number between 1 and {} to download questionnaire, 'q' to exit: ".format(c_qlist)
+    while True:
+        choice = input(msg)
+        if choice == "q":
+            sys.exit()
+        try:
+            choice = int(choice)
+        except:
+            continue
+        if 1 <= choice <= c_qlist:
+            break
+    return qlist[choice-1]
+
+if __name__ == "__main__":
+    args = process_args()
+
+    from ssaw.headquarters import Headquarters
+
+    print(args.password)
+    client = Headquarters(args.url, args.username, args.password)
+    if args.qid is None:
+        q = get_questionnaire(client)
+        print()
+        get_export(client, q.questionnaire_id, q.version, args.exportpath)
+    else:
+        get_export(client, args.qid, args.version, args.exportpath)
